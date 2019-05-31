8985f70bd16d4c56995b714128547ca8f70ad82a
JavaPsyh <psyhjava@gmail.com>
2019-05-31 16:43:00 +0300

    commit of branch forPull

--- src/psyh/PatternBuilder.java
+++ src/psyh/PatternBuilder.java
@@ -1,3 +1,59 @@
 package psyh;
 
+class ComputerBuilder {
+
+    private String monitor;
+    private String systembox;
+    private String manipulator;
+
+
+    public String getMonitor() {
+        return monitor;
+    }
+
+    public String getSystembox() {
+        return systembox;
+    }
+
+    public String getManipulator() {
+        return manipulator;
+    }
+
+
+    public static class Builder {
+
+        private ComputerBuilder computerBuilder;
+
+        public Builder() {
+            computerBuilder = new ComputerBuilder();
+        }
+
+        public Builder withMonitor(String monitor) {
+            computerBuilder.monitor = monitor;
+            return this;
+        }
+
+        public Builder withSystembox(String systembox) {
+            computerBuilder.systembox = systembox;
+            return this;
+        }
+
+        public Builder withManipulator(String manipulator) {
+            computerBuilder.manipulator = manipulator;
+            return this;
+        }
+
+        public ComputerBuilder build() {
+            return computerBuilder;
+        }
+    }
+
+    public static void main(String[] args) {
+        ComputerBuilder computerBuilder = new ComputerBuilder.Builder()
+                .withManipulator("Keyboard")
+                .withMonitor("LCD monitor")
+                .withSystembox("Black systembox")
+                .build();
+    }
+}
 
